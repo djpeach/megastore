85cdb7033cc47ae4d393dd65fc3bacb1b33bad25
Daniel Peach <djpeach96@gmail.com>
2023-10-04 21:39:03 -0400

    initializer

---
+++ src/main/kotlin/com/example/megastore/MegastoreApplication.kt
@@ -0,0 +1,11 @@
+package com.example.megastore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MegastoreApplication
+
+fun main(args: Array<String>) {
+  runApplication<MegastoreApplication>(*args)
+}
